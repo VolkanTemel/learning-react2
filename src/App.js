d7f513070a7f69c4490afb20812bd33b99086e18
Volkan Temel <sapphiron07@windowslive.com>
2020-11-22 18:45:15 +0300

    remove styled components from app.js

--- src/App.js
+++ src/App.js
@@ -45,8 +45,12 @@ render(){
     font: 'inherit',
     border: '1px solid blue',
     padding: '8px',
-    cursor: 'pointer'
-  }
+    cursor: 'pointer',
+    ':hover':{
+      backgroundColor: 'lightgreen',
+      color: 'black'
+    }
+  };
   let persons=null;
   if(this.state.showPersons){
 persons=(<div>
@@ -60,7 +64,11 @@ persons=(<div>
    />
  })}
   </div>);
-  style.backgroundColor= 'red';
+  // style.backgroundColor= 'red';
+  // style[':hover']={
+  //   backgroundColor: 'salmon',
+  //   color: 'black'
+  // }
   }
   let classes = [];
   if (this.state.persons.length<=2){
@@ -74,7 +82,7 @@ persons=(<div>
       <div className="App">
        <h1>Hi, I'm a React App</h1>
        <p className={classes.join(' ')}>This is really working!</p>
-       <button style={style} onClick={this.togglePersonsHandler}>Toggle Persons</button>
+       <button className="button" onClick={this.togglePersonsHandler}>Toggle Persons</button>
       {persons}
        
       </div>
